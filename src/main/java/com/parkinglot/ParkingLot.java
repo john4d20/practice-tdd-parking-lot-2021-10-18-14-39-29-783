57269be650a42740df70274d554984e8afcf8a09
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-03 14:08:18 +0800

    [parking lot][john]feak: test pass should_return_ticket_when_park_car_given_parking_lot_and_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 
 public class ParkingLot {
     public Ticket park(Car car){
-//        HashMap<Ticket,Car> ticketCarHashMap = new HashMap<>();
-//        Ticket ticket = new Ticket();
-//        ticketCarHashMap.put(ticket,car);
-//
-//        return ticket;
-        return null;
+        HashMap<Ticket,Car> ticketCarHashMap = new HashMap<>();
+        Ticket ticket = new Ticket();
+        ticketCarHashMap.put(ticket,car);
+
+        return ticket;
+
     }
 }
