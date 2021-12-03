02299444c303b4e2830ccb69dd167136c9ef49ad
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-03 14:35:48 +0800

    [parking lot][john]feak: test pass should_return_right_car_with_each_ticket_when_fetch_car_given_two_cars_and_two_tickets

--- src/test/java/com/parkinglot/test.java
+++ src/test/java/com/parkinglot/test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class test {
+//    case1
     @Test
     void should_return_ticket_when_park_car_given_parking_lot_and_car() {
         //given
@@ -16,7 +17,7 @@ public class test {
         //then
         assertNotNull(ticket);
     }
-    
+//    case2
     @Test
     void should_return_null_when_park_car_given_full_parking_lot_and_car() {
         //given
@@ -27,7 +28,7 @@ public class test {
         //then
         assertNull(ticket);
     }
-
+//case3
     @Test
     void should_return_car_when_fetch_car_given_parking_lot_and_car_and_ticket() {
         //given
@@ -39,5 +40,22 @@ public class test {
         //then
         assertEquals(car,fetchedCar);
     }
+//    case4
+    @Test
+    void should_return_right_car_with_each_ticket_when_fetch_car_given_two_cars_and_two_tickets() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Car car2 = new Car();
+        Ticket ticket = parkingLot.park(car);
+        Ticket ticket2 = parkingLot.park(car2);
+        //when
+        Car fetchedCar = parkingLot.fetch(ticket);
+        Car fetchedCar2 = parkingLot.fetch(ticket2);
+        //then
+        assertEquals(car,fetchedCar);
+        assertEquals(car2,fetchedCar2);
+    }
+
 
 }
