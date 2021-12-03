d4aec4de79100d2463a65ec67357b300bc3c950d
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-03 14:12:41 +0800

    [parking lot][john]feak: test should_return_null_when_park_car_given_full_parking_lot_and_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -3,6 +3,15 @@ package com.parkinglot;
 import java.util.HashMap;
 
 public class ParkingLot {
+    int capacity;
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public ParkingLot() {
+
+    }
+
     public Ticket park(Car car){
         HashMap<Ticket,Car> ticketCarHashMap = new HashMap<>();
         Ticket ticket = new Ticket();
--- src/test/java/com/parkinglot/test.java
+++ src/test/java/com/parkinglot/test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class test {
     @Test
@@ -17,5 +18,17 @@ public class test {
         //then
         assertNotNull(ticket);
     }
+    
+    @Test
+    void should_return_null_when_park_car_given_full_parking_lot_and_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(1);
+        Car car = new Car();
+        //when
+        Ticket ticket = parkingLot.park(car);
+        //then
+        assertNull(ticket);
+    }
+    
 
 }
