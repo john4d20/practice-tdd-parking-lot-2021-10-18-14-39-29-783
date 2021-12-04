40968b8bab0d979e6776f28d1a2896bcfbf8e53b
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-04 20:27:46 +0800

    [parking lot][john]feat: test pass should_return_no_available_message_when_park_car_given_smart_boy_and_full_parking_lots

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class SmartParkingBoy extends ParkingBoy{
+    private final String NO_AVAILABLE_POSITION = "No available position";
     public SmartParkingBoy(ArrayList<ParkingLot> parkingLots) {
         super(parkingLots);
     }
 
     @Override
-    public Ticket park(Car car)  {
+    public Ticket park(Car car)  throws NoAvailablePositionException{
         ParkingLot emptyParkingLot = this.getParkingLots().stream().max(
                 Comparator.comparing(ParkingLot::getAvailablePosition)).orElse(null);
         if (emptyParkingLot != null) {
             return emptyParkingLot.park(car);
         }
-        return null;
+        throw new NoAvailablePositionException(NO_AVAILABLE_POSITION);
     }
 
 }
