18ad15d78d012b3394ced6f3b020597335dcc227
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-04 19:24:12 +0800

    [parking lot][john]feat: test pass should_return_right_car_when_fetch_car_given_two_parked_car

--- src/test/java/com/parkinglot/PakingBoyTest.java
+++ src/test/java/com/parkinglot/PakingBoyTest.java
@@ -45,21 +45,26 @@ public class PakingBoyTest {
     }
 
     @Test
-    void should_return_when_park_car_given_full_first_parking_lot_and_second_parking_lot() {
+    void should_return_right_car_when_fetch_car_given_two_parked_car() {
         //given
-        ParkingLot firstParkingLot = new ParkingLot(0);
+        ParkingLot firstParkingLot = new ParkingLot();
         ParkingLot secondParkingLot = new ParkingLot();
         ArrayList<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(firstParkingLot);
         parkingLots.add(secondParkingLot);
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
-
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
         //when
-        Ticket ticket = parkingBoy.park(new Car());
+        Car fectchedCar = parkingBoy.fetch(ticket);
+        Car fectchedCar2 = parkingBoy.fetch(ticket2);
+
         //then
-        assertNotNull(ticket);
-        assertEquals(0,firstParkingLot.getAvailablePosition());
-        assertEquals(9,secondParkingLot.getAvailablePosition());
+
+        assertEquals(car1,fectchedCar);
+        assertEquals(car2,fectchedCar2);
     }
 
 }
