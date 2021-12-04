94131822d5b7e4fdcd4eb75d894a69015071bb31
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-04 20:18:25 +0800

    [parking lot][john]feat: test pass should_park_to_first_parking_lot_when_park_car_given_smart_parking_boy_manage_two_parking_lots_second_lots_have_more_empty

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SmartParkingBoyTest {
     @Test
-    void should_park_to_first_parking_lot_when_park_car_given_smart_parking_boy_manage_two_parking_lots_bot_available() {
+    void should_park_to_first_parking_lot_when_park_car_given_smart_parking_boy_manage_two_parking_lots_both_available() {
         //given
         ParkingLot firstParkingLot = new ParkingLot();
         ParkingLot secondParkingLot = new ParkingLot();
@@ -24,5 +24,23 @@ public class SmartParkingBoyTest {
         assertNotNull(ticket);
         assertEquals(9,firstParkingLot.getAvailablePosition());
         assertEquals(10,secondParkingLot.getAvailablePosition());
+    }
+
+    @Test
+    void should_park_to_first_parking_lot_when_park_car_given_smart_parking_boy_manage_two_parking_lots_second_lots_have_more_empty() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        //when
+        Ticket ticket = smartParkingBoy.park(new Car());
+        //then
+        assertNotNull(ticket);
+        assertEquals(1,firstParkingLot.getAvailablePosition());
+        assertEquals(1,secondParkingLot.getAvailablePosition());
     }
 }
