b2fe25aeb954a5e730c2ac7f5258d130164b2d2f
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-04 19:46:16 +0800

    [parking lot][john]feat: test pass should_return_error_message_when_fetch_car_given_unrecognized_ticket

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class ParkingBoy {
     private List<ParkingLot> parkingLots;
+    private final String NO_AVAILABLE_POSITION = "No available position";
+    private final String UNRECOGNIZED_PARKING_TICKET = "Unrecognized parking ticket";
 
     public ParkingBoy(ArrayList<ParkingLot> parkingLots) {
         this.parkingLots = parkingLots;
@@ -18,10 +20,14 @@ public class ParkingBoy {
     }
 
     public Car fetch(Ticket ticket) throws UnrecognizedParkingTicketException {
-        for (ParkingLot parkingLot : parkingLots) {
-            return parkingLot.fetch(ticket);
+        for (ParkingLot parkingLot: parkingLots) {
+            try {
+                return parkingLot.fetch(ticket);
+            }
+            catch (UnrecognizedParkingTicketException ignored) {
 
+            }
         }
-        return null;
+        throw new UnrecognizedParkingTicketException(UNRECOGNIZED_PARKING_TICKET);
     }
 }
