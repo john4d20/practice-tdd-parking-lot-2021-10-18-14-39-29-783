e1379e70ca905a9e62968937fb610d19857a4039
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-04 21:11:25 +0800

    [parking lot][john]feat: test should_return_right_car_when_fetch_car_given__super_smart_parking_boy_and_two_parked_car

--- src/test/java/com/parkinglot/SuperSmartboyparkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartboyparkingBoyTest.java
@@ -44,4 +44,27 @@ public class SuperSmartboyparkingBoyTest {
         assertEquals(9,firstParkingLot.getAvailablePosition());
         assertEquals(9,secondParkingLot.getAvailablePosition());
     }
+
+    @Test
+    void should_return_right_car_when_fetch_car_given__super_smart_parking_boy_and_two_parked_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket = superSmartParkingBoy.park(car1);
+        Ticket ticket2 = superSmartParkingBoy.park(car2);
+        //when
+        Car fectchedCar = superSmartParkingBoy.fetch(ticket);
+        Car fectchedCar2 = superSmartParkingBoy.fetch(ticket2);
+
+        //then
+
+        assertEquals(car1,fectchedCar);
+        assertEquals(car2,fectchedCar2);
+    }
 }
