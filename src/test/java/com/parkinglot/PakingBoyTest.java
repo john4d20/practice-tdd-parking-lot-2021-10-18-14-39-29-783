17a5c19290774220f89841ce46439b4b771ab80d
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-04 19:23:50 +0800

    [parking lot][john]feat: test pass should_park_to_second_parking_lot_when_park_car_given_full_first_parking_lot_and_second_parking_lot

--- src/test/java/com/parkinglot/PakingBoyTest.java
+++ src/test/java/com/parkinglot/PakingBoyTest.java
@@ -44,4 +44,22 @@ public class PakingBoyTest {
         assertEquals(9,secondParkingLot.getAvailablePosition());
     }
 
+    @Test
+    void should_return_when_park_car_given_full_first_parking_lot_and_second_parking_lot() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(0);
+        ParkingLot secondParkingLot = new ParkingLot();
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+
+        //when
+        Ticket ticket = parkingBoy.park(new Car());
+        //then
+        assertNotNull(ticket);
+        assertEquals(0,firstParkingLot.getAvailablePosition());
+        assertEquals(9,secondParkingLot.getAvailablePosition());
+    }
+
 }
